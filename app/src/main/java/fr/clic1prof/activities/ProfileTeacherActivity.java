b6d72984346b65b103d3d6865069fc7a6c1aef5b
CelestialTios <geraud.bertrand@gmail.com>
2020-12-14 09:39:47 +0100

    Udpate from dev

---
+++ app/src/main/java/fr/clic1prof/activities/ProfileTeacherActivity.java
@@ -0,0 +1,16 @@
+package fr.clic1prof.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import fr.clic1prof.R;
+
+public class ProfileTeacherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_teacher_layout);
+    }
+}
