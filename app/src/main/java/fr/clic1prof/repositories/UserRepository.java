a67391dd74d6631088821e57f569ee475ee87b8c
Syrows <syrows.development@gmail.com>
2020-12-15 19:06:00 +0100

    Adding Result class for LiveData. Improving contact and login viewmodels

--- app/src/main/java/fr/clic1prof/repositories/UserRepository.java
+++ app/src/main/java/fr/clic1prof/repositories/UserRepository.java
@@ -1,12 +1,15 @@
 package fr.clic1prof.repositories;
 
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
 import fr.clic1prof.models.user.Credentials;
 import fr.clic1prof.models.user.Registration;
+import fr.clic1prof.models.user.Token;
 import fr.clic1prof.models.user.UserSessionModel;
+import fr.clic1prof.viewmodels.Result;
 
 public interface UserRepository {
 
-    void login(MutableLiveData<UserSessionModel> data, Credentials credentials);
+    LiveData<Token> login(Credentials credentials);
 }
