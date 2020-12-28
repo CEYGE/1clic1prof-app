962a8e417d6cdf97101002811bd31aedbb8ebab2
Syrows <syrows.development@gmail.com>
2020-12-28 21:02:02 +0100

    Fixing session type bug.

--- app/src/main/java/fr/clic1prof/network/authentication/AuthenticationResponse.java
+++ app/src/main/java/fr/clic1prof/network/authentication/AuthenticationResponse.java
@@ -1,10 +1,15 @@
 package fr.clic1prof.network.authentication;
 
+import com.google.gson.annotations.SerializedName;
+
 import fr.clic1prof.models.session.SessionType;
 
 public class AuthenticationResponse {
 
+    @SerializedName("token")
     private final String token;
+
+    @SerializedName("role")
     private final SessionType type;
 
     public AuthenticationResponse(String token, SessionType type) {
