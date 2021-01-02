0345c627367a20968195770dbae3833f9d81a816
Syrows <syrows.development@gmail.com>
2021-01-02 22:05:03 +0100

    Updating profile.

--- app/src/main/java/fr/clic1prof/models/profile/Profile.java
+++ app/src/main/java/fr/clic1prof/models/profile/Profile.java
@@ -6,13 +6,14 @@ public class Profile {
 
     private final String email;
     private String firstName, lastName;
+    private int pictureId;
     private Bitmap picture;
 
-    public Profile(String email, String firstName, String lastName, Bitmap picture) {
+    public Profile(String email, String firstName, String lastName, int pictureId) {
         this.email = email;
         this.firstName = firstName;
         this.lastName = lastName;
-        this.picture = picture;
+        this.pictureId = pictureId;
     }
 
     public String getEmail() {
@@ -33,6 +34,14 @@ public class Profile {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public int getPictureId() {
+        return this.pictureId;
+    }
+
+    public void setPictureId(int pictureId) {
+        this.pictureId = pictureId;
     }
 
     public Bitmap getPicture() {
--- app/src/main/java/fr/clic1prof/models/profile/StudentProfile.java
+++ app/src/main/java/fr/clic1prof/models/profile/StudentProfile.java
@@ -1,15 +1,13 @@
 package fr.clic1prof.models.profile;
 
-import android.graphics.Bitmap;
-
 import fr.clic1prof.models.other.SchoolLevel;
 
 public class StudentProfile extends Profile {
 
     private SchoolLevel level;
 
-    public StudentProfile(String email, String firstName, String lastName, Bitmap picture, SchoolLevel level) {
-        super(email, firstName, lastName, picture);
+    public StudentProfile(String email, String firstName, String lastName, int pictureId, SchoolLevel level) {
+        super(email, firstName, lastName, pictureId);
         this.level = level;
     }
 
